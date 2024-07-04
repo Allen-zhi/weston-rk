Allen-zhi/weston

desktop-shell: Delay locking when having pending fade-out animations
Avoid "unexpectedly large timestamp jump" warning when resuming with
multi-head.

Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

desktop-shell/shell.c
@@ -3580,18 +3580,24 @@ shell_fade_done_for_output(struct weston_view_animation *animation, void *data)
 {
 	struct shell_output *shell_output = data;
 	struct desktop_shell *shell = shell_output->shell;
+	struct weston_compositor *compositor = shell->compositor;
+
+	if (shell_output->fade.type == FADE_OUT)
+		shell->compositor->pending_fade_out --;
 
 	if (!shell_output->fade.curtain)
 		return;
 
 	shell_output->fade.animation = NULL;
+
 	switch (shell_output->fade.type) {
 	case FADE_IN:
 		weston_curtain_destroy(shell_output->fade.curtain);
 		shell_output->fade.curtain = NULL;
 		break;
 	case FADE_OUT:
-		lock(shell);
+		if (!compositor->pending_fade_out)
+			lock(shell);
 		break;
 	default:
 		break;
@@ -3705,6 +3711,9 @@ shell_fade(struct desktop_shell *shell, enum fade_type type)
 		} else if (shell_output->fade.animation) {
 			weston_fade_update(shell_output->fade.animation, tint);
 		} else {
+			if (type == FADE_OUT)
+				shell->compositor->pending_fade_out ++;
+
 			shell_output->fade.animation =
 				weston_fade_run(shell_output->fade.curtain->view,
 						1.0 - tint, tint, 300.0,
@@ -4371,6 +4380,9 @@ shell_output_destroy(struct shell_output *shell_output)
 	if (shell_output->fade.animation) {
 		weston_view_animation_destroy(shell_output->fade.animation);
 		shell_output->fade.animation = NULL;
+
+		if (shell_output->fade.type == FADE_OUT)
+			shell->compositor->pending_fade_out --;
 	}
 
 	if (shell_output->fade.curtain) {

Allen-zhi/weston-rk

desktop-shell/shell.c
@@ -3891,6 +3891,10 @@ static void
 shell_fade_done(struct weston_view_animation *animation, void *data)
 {
 	struct desktop_shell *shell = data;
+	struct weston_compositor *compositor = shell->compositor;
+
+	if (shell->fade.type == FADE_OUT)
+		compositor->pending_fade_out --;
 
 	if (!shell->fade.curtain)
 		return;
@@ -3902,7 +3906,8 @@ shell_fade_done(struct weston_view_animation *animation, void *data)
 		shell->fade.curtain = NULL;
 		break;
 	case FADE_OUT:
-		lock(shell);
+		if (!compositor->pending_fade_out)
+			lock(shell);
 		break;
 	default:
 		break;
@@ -3992,6 +3997,9 @@ shell_fade(struct desktop_shell *shell, enum fade_type type)
 	if (shell->fade.animation) {
 		weston_fade_update(shell->fade.animation, tint);
 	} else {
+		if (type == FADE_OUT)
+			shell->compositor->pending_fade_out ++;
+
 		shell->fade.animation =
 			weston_fade_run(shell->fade.curtain->view,
 					1.0 - tint, tint, 300.0,
@@ -4901,6 +4909,9 @@ shell_destroy(struct wl_listener *listener, void *data)
 	if (shell->fade.animation) {
 		weston_view_animation_destroy(shell->fade.animation);
 		shell->fade.animation = NULL;
+
+		if (shell->fade.type == FADE_OUT)
+			shell->compositor->pending_fade_out --;
 	}
 
 	if (shell->fade.curtain) {
ginkage/weston

backend-drm: Support delaying initial repaint
Set env "WESTON_DRM_INITIAL_FREEZE_MS" for the initial freezing.

Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

libweston/backend-drm/drm-internal.h
@@ -408,6 +408,9 @@ struct drm_backend {
 	int64_t last_resize_ms;
 	int64_t resize_freeze_ms;
 
+	int64_t initial_repaint_ms;
+	int64_t initial_freeze_ms;
+
 	bool master;
 	bool single_head;
 	bool head_fallback;

Allen-zhi/weston-rk

libweston/backend-drm/drm-internal.h
@@ -402,6 +402,9 @@ struct drm_backend {
 	bool pending_update;
 	int64_t last_update_ms;
 
+	int64_t initial_update_ms;
+	int64_t initial_freeze_ms;
+
 	bool master;
 	bool single_head;
 	bool head_fallback;